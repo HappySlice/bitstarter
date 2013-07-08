ca80a75a21a506441056262ba77f83b2dbfbdbf6
HappySlice <david.busch@gmail.com>
2013-07-08 04:14:05 +0000

    Added fs.writeFileSync

--- web.js
+++ web.js
@@ -2,8 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
+});
+
+fs.writeFileSync('index.html', 'Hello Node', function (err) {
+  if (err) throw err;
+  console.log('It\'s saved!');
 });
 
 var port = process.env.PORT || 5000;
